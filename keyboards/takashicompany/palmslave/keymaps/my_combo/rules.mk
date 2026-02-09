6e48f7eb68c4d4cb4612f1a1a8a97125e3bbc530
Iwarina <thomas23454@gmail.com>
2026-02-10 00:13:17 +0900

    Update rules.mk

--- keyboards/takashicompany/palmslave/keymaps/my_combo/rules.mk
+++ keyboards/takashicompany/palmslave/keymaps/my_combo/rules.mk
@@ -1,9 +1,3 @@
-# コンボ機能を有効化
 COMBO_ENABLE = yes
 
-# 【重要】ここから下を追加
-# QMKに「これはCaterinaブートローダ(ProMicro)だよ」と強制認識させる
-BOOTLOADER = caterina
-
-# BMP用に変換する指示をここに書く
-CONVERT_TO = promicro_nrf52
+# 他の記述（BOOTLOADERやCONVERT_TO）は全て消してください
